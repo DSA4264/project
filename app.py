0347aa3b831d5f3dabc30705666365be35d185f2
zhyoung17 <122663876+zhyoung17@users.noreply.github.com>
2024-10-26 22:36:45 +0800

    Added softcoding for each section

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('scrollytelling.html')
+def scrollytelling():
+    # Define the path to sections.json
+    data_dir = os.path.join(app.root_path, 'data')
+    sections_file = os.path.join(data_dir, 'sections.json')
+
+    # Load sections.json
+    with open(sections_file, 'r', encoding='utf-8') as f:
+        sections = json.load(f)
+    return render_template('scrollytelling.html', sections=sections)
 
 if __name__ == '__main__':
     app.run(debug=True)
